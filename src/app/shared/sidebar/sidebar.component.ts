08f15b2fe1e7ca4b227996603dbec41bf99d88e3
juan Manuel <jmgv87191@gmail.com>
2024-08-16 12:22:42 -0700

    asd

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component,  } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent  {
+
+  constructor( private gifService: GifsService ){
+  }
+  
+  get tags(){
+    return  this.gifService.tagHistory;
+  }
+
+  
 
 }
