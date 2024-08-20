c820e7b59a649b36f4569dd9a34ebcbb837dd0a5
juan Manuel <jmgv87191@gmail.com>
2024-08-20 13:19:18 -0700

    asd

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Gif } from '../../interfaces/gifs';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-card-list',
   standalone: true,
-  imports: [],
+  imports: [ CardComponent ],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.css'
 })
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    if (!this.item) throw new Error('Gif property is required')
+      
+  }
+
+  @Input() public item!: Gif;
+
+}
